40e0faecc8db47682ff277e33fc4c8ece4198f05
vibing <ijaasahamed2003@gmail.com>
2025-11-01 21:41:20 +0530

    application properties changes

--- src/main/java/com/ratelimit/ratelimit/RatelimitApplication.java
+++ src/main/java/com/ratelimit/ratelimit/RatelimitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RatelimitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RatelimitApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RatelimitApplication.class, args);
+  }
 
 }
