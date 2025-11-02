6225b8735c407623e76ca83be39510a0b810302c
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-02 22:34:45 +0530

    XMLParser change'

--- src/main/java/com/ratelimit/ratelimit/Interceptors/Parser/XMLParser.java
+++ src/main/java/com/ratelimit/ratelimit/Interceptors/Parser/XMLParser.java
@@ -2,6 +2,9 @@ package com.ratelimit.ratelimit.Interceptors.Parser;
 
 import java.io.File;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.ratelimit.ratelimit.Model.UrlWrapper;
 
@@ -15,6 +18,8 @@ import com.ratelimit.ratelimit.Model.UrlWrapper;
  *          store the data in {@value Redis}
  *
  **/
+
+@Component
 public class XMLParser {
   private static final String xmlFilePath = "/Users/ijaas-22254/learn/project/ratelimit/src/main/resources/api.xml";
 
@@ -22,9 +27,18 @@ public class XMLParser {
    * This is method whicj we are using to parse the
    * {@link com.ratelimit.ratelimit.Interceptors.Parser.XMLParser}
    **/
+
+  @Autowired
+  public XMLParserUtil xmlParserUtil;
+
   public void parse() throws Exception {
     XmlMapper xmlMapper = new XmlMapper();
     UrlWrapper urlWrapper = xmlMapper.readValue(new File(xmlFilePath), UrlWrapper.class);
-
+    boolean isAdded = xmlParserUtil.storeURLInStore(urlWrapper);
+    if (isAdded) {
+      System.out.println("Data Being populated");
+    } else {
+      System.out.println("Data Not Being populated");
+    }
   }
 }
