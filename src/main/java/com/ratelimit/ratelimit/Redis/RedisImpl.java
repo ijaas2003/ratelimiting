17b8c72c885b6b74af7a0778d8fafa9322718e9f
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-02 16:07:10 +0530

    Redis Implemention

---
+++ src/main/java/com/ratelimit/ratelimit/Redis/RedisImpl.java
@@ -0,0 +1,10 @@
+package com.ratelimit.ratelimit.Redis;
+
+import java.util.Optional;
+
+public class RedisImpl implements RedisAPI {
+  @Override
+  public Optional<?> get(String key) {
+    return Optional.empty();
+  }
+}
