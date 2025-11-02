be07cd267d6cbc1114c8d6cc63d2756df2cb1f22
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-02 22:36:02 +0530

    URK mapper change

--- src/main/java/com/ratelimit/ratelimit/RatelimitApplication.java
+++ src/main/java/com/ratelimit/ratelimit/RatelimitApplication.java
@@ -3,14 +3,10 @@ package com.ratelimit.ratelimit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ratelimit.ratelimit.Interceptors.Parser.XMLParser;
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ratelimit.ratelimit")
 public class RatelimitApplication {
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(RatelimitApplication.class, args);
-    new XMLParser().parse();
   }
-
 }
