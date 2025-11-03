3fce5112e8c23e3f63d0987a219e144adb70f43f
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-03 22:03:51 +0530

    Interceptor implementation

--- src/main/java/com/ratelimit/ratelimit/Interceptors/RateLimiterInterceptor.java
+++ src/main/java/com/ratelimit/ratelimit/Interceptors/RateLimiterInterceptor.java
@@ -1,5 +1,7 @@
 package com.ratelimit.ratelimit.Interceptors;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -17,7 +19,16 @@ public class RateLimiterInterceptor implements HandlerInterceptor {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-    xmlParserUtil.validateUrl(request.getRequestURI(), request.getMethod());
+    boolean isValid = xmlParserUtil.validateUrl(request.getRequestURI(), request.getMethod());
+    if (!isValid) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      response.setContentType("application/json");
+      try {
+        response.getWriter().write("Access Denied");
+      } catch (IOException e) {
+        System.out.println("Exception Occured");
+      }
+    }
     return true;
   }
 }
