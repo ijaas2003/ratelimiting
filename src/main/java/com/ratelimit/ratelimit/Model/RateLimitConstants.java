49f52ae805feeeb0cf75a97f360096ea86209f80
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-05 00:11:04 +0530

    Add Blocked constants

--- src/main/java/com/ratelimit/ratelimit/Model/RateLimitConstants.java
+++ src/main/java/com/ratelimit/ratelimit/Model/RateLimitConstants.java
@@ -3,6 +3,7 @@ package com.ratelimit.ratelimit.Model;
 public class RateLimitConstants {
   public static final String SLIDING_WINDOW = "slidingwindow";
   public static final String FIXED_WINDOW = "fixedwindow";
+  public static final String BLOCKED = "_blocked";
 
   public enum WindowTime {
     MINITUS,
