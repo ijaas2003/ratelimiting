fad72c2e47ab487f15802b9ae882315708240798
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-05 00:13:12 +0530

    Redis Impl changes

--- src/main/java/com/ratelimit/ratelimit/Redis/RedisImpl.java
+++ src/main/java/com/ratelimit/ratelimit/Redis/RedisImpl.java
@@ -1,5 +1,6 @@
 package com.ratelimit.ratelimit.Redis;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.params.ScanParams;
+import redis.clients.jedis.resps.ScanResult;
 
 @Service
 public class RedisImpl implements RedisAPI {
@@ -16,7 +19,7 @@ public class RedisImpl implements RedisAPI {
   private JedisPool jedisPool;
 
   @Override
-  public Optional<?> get(String key) {
+  public Optional<String> get(String key) {
     try (Jedis jedis = jedisPool.getResource()) {
       if (jedis.exists(key)) {
         String value = jedis.get(key);
@@ -46,5 +49,21 @@ public class RedisImpl implements RedisAPI {
         jedis.expire(key, TTL);
       }
     }
+  }
+
+  @Override
+  public Optional<Long> getUsingScan(final String key) {
+    Long totalUsed = 0l;
+    try (Jedis jedis = jedisPool.getResource()) {
+      String cursor = new ScanParams().SCAN_POINTER_START;
+      ScanParams param = new ScanParams().match(key).count(10000);
+      do {
+        ScanResult<String> result = jedis.scan(cursor, param);
+        List<String> resultList = result.getResult();
+        totalUsed += resultList.size();
+        cursor = result.getCursor();
+      } while (!cursor.equals("0"));
+    }
+    return Optional.of(totalUsed);
   }
 }
