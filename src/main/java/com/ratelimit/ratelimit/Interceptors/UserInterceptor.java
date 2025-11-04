2b3eda680ee237e46305f62a5e69cadaf708e1a1
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-04 22:43:57 +0530

    Thread local set in interceptor

--- src/main/java/com/ratelimit/ratelimit/Interceptors/UserInterceptor.java
+++ src/main/java/com/ratelimit/ratelimit/Interceptors/UserInterceptor.java
@@ -1,22 +1,17 @@
 package com.ratelimit.ratelimit.Interceptors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.ratelimit.ratelimit.ThreadLocal.*;
 
 public class UserInterceptor implements HandlerInterceptor {
 
-  @Autowired
-  private UserThread userThread;
-
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) {
     String s = request.getCookies().toString();
-
+    UserThread.setUserId(s);
     return true;
   }
 }
