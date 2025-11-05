c5ab9599b7a135cb446c7e642b1ced1ba7024fa7
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-06 01:11:18 +0530

    uSER

--- src/main/java/com/ratelimit/ratelimit/Interceptors/UserInterceptor.java
+++ src/main/java/com/ratelimit/ratelimit/Interceptors/UserInterceptor.java
@@ -2,18 +2,27 @@ package com.ratelimit.ratelimit.Interceptors;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.ratelimit.ratelimit.ThreadLocal.*;
+
+import io.micrometer.common.lang.Nullable;
 
 @Component
 public class UserInterceptor implements HandlerInterceptor {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) {
-    String s = request.getCookies().toString();
+    String s = request.getCookies()[0].getValue();
     UserThread.setUserId(s);
     return true;
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object object,
+      @Nullable ModelAndView modelAndView) {
+    UserThread.clearUserId();
   }
 }
