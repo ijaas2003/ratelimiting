5dc7a2015dea9617424cbe4651d98215590526ca
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-06 01:09:26 +0530

    Redis Service change

--- src/main/java/com/ratelimit/ratelimit/Controller/GlobalController.java
+++ src/main/java/com/ratelimit/ratelimit/Controller/GlobalController.java
@@ -1,7 +1,28 @@
 package com.ratelimit.ratelimit.Controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class GlobalController {
+
+  @GetMapping("/users")
+  public ResponseEntity<Map<String, Object>> getUsers() {
+    Map<String, Object> response = new HashMap<>();
+    response.put("status", "success");
+    response.put("message", "User data fetched successfully");
+    response.put("count", 5);
+
+    return ResponseEntity
+        .status(HttpStatus.OK) // sets status 202
+        .body(response); // sets body
+  }
+
 }
