7077096c0ce9db336df32906455a1da5aeac3057
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-06 01:11:36 +0530

    uSER

--- src/main/java/com/ratelimit/ratelimit/Model/RateLimitConstants.java
+++ src/main/java/com/ratelimit/ratelimit/Model/RateLimitConstants.java
@@ -5,6 +5,7 @@ public class RateLimitConstants {
   public static final String FIXED_WINDOW = "fixedwindow";
   public static final String BLOCKED = "_blocked";
   public static final String WINDOW_REGEX = "(\\d+)([smhd])";
+  public static final String ERROR_MESSAGE_LIMIT_REACHED = "api limit reached";
 
   public enum WindowTime {
     MINITUS,
