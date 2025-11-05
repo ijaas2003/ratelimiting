cc14572b0f8824892f97ae0fa890a7fb2ba7e846
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-06 01:09:50 +0530

    New response object created

---
+++ src/main/java/com/ratelimit/ratelimit/Interceptors/Response.java
@@ -0,0 +1,20 @@
+package com.ratelimit.ratelimit.Interceptors;
+
+import org.json.JSONObject;
+
+public class Response {
+  public static enum Type {
+    SUCCESS,
+    ERROR
+  }
+
+  public static JSONObject getResponse(String message, Type type) {
+    JSONObject json = new JSONObject();
+    JSONObject res = new JSONObject();
+    JSONObject mess = new JSONObject();
+    mess.put("message", message);
+    res.put(type.toString(), mess);
+    json.put("Response", res);
+    return json;
+  }
+}
