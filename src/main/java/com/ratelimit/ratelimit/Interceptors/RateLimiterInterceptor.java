81b86ef27cf0146bc58629d332f67bae3cc8e22d
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-06 01:10:48 +0530

    Interceptor

--- src/main/java/com/ratelimit/ratelimit/Interceptors/RateLimiterInterceptor.java
+++ src/main/java/com/ratelimit/ratelimit/Interceptors/RateLimiterInterceptor.java
@@ -1,10 +1,14 @@
 package com.ratelimit.ratelimit.Interceptors;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.ratelimit.ratelimit.Interceptors.Parser.XMLParserUtil;
+import com.ratelimit.ratelimit.Model.RateLimitConstants;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,7 +21,20 @@ public class RateLimiterInterceptor implements HandlerInterceptor {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-    xmlParserUtil.validateUrl(request.getRequestURI(), request.getMethod());
+    boolean rateLimitReached = xmlParserUtil.validateUrl(request.getRequestURI(), request.getMethod());
+    if (!rateLimitReached) {
+      response.setStatus(HttpStatus.NOT_FOUND.value());
+      response.setContentType("application/json");
+      try {
+        response.getWriter()
+            .write(
+                Response.getResponse(RateLimitConstants.ERROR_MESSAGE_LIMIT_REACHED, Response.Type.ERROR).toString());
+
+      } catch (IOException e) {
+        System.out.println(e);
+      }
+      return false;
+    }
     return true;
   }
 }
