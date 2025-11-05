9ebecafe61a7bce071d5c9fb3edf6ff1b1bbeaa5
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-05 22:38:17 +0530

    RateLimiter change

--- src/main/java/com/ratelimit/ratelimit/Model/RateLimitConstants.java
+++ src/main/java/com/ratelimit/ratelimit/Model/RateLimitConstants.java
@@ -4,6 +4,7 @@ public class RateLimitConstants {
   public static final String SLIDING_WINDOW = "slidingwindow";
   public static final String FIXED_WINDOW = "fixedwindow";
   public static final String BLOCKED = "_blocked";
+  public static final String WINDOW_REGEX = "(\\d+)([smhd])";
 
   public enum WindowTime {
     MINITUS,
