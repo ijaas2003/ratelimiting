1939a4317db7a135416d6a0f844d904c7c4c3982
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-09 17:08:48 +0530

    Implementation for other api

--- src/main/java/com/ratelimit/ratelimit/Controller/GlobalController.java
+++ src/main/java/com/ratelimit/ratelimit/Controller/GlobalController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,19 @@ public class GlobalController {
     return ResponseEntity
         .status(HttpStatus.OK) // sets status 202
         .body(response); // sets body
+  }
+
+  @GetMapping("/{userId}/getUsersDetails")
+  public ResponseEntity<Map<String, Object>> getUsersDetails(@PathVariable("userId") Long userId) {
+    Map<String, Object> response = new HashMap<>();
+    response.put("status", "success");
+    response.put("message", "User data fetched successfully");
+    response.put("count", 5);
+
+    return ResponseEntity
+        .status(HttpStatus.OK) // sets status 202
+        .body(response); // sets body
+
   }
 
 }
