6b778eb77cfbab9756b70ab56e598922b91cb595
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-09 17:11:16 +0530

    Implementaion for interceptor

--- src/main/java/com/ratelimit/ratelimit/Interceptors/UserInterceptor.java
+++ src/main/java/com/ratelimit/ratelimit/Interceptors/UserInterceptor.java
@@ -1,11 +1,17 @@
 package com.ratelimit.ratelimit.Interceptors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import com.ratelimit.ratelimit.Model.Response;
+import com.ratelimit.ratelimit.Model.ResponseConstants;
+import com.ratelimit.ratelimit.Model.ResponseConstants.Type;
 import com.ratelimit.ratelimit.ThreadLocal.*;
 
 import io.micrometer.common.lang.Nullable;
@@ -15,8 +21,16 @@ public class UserInterceptor implements HandlerInterceptor {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) {
-    String s = request.getCookies()[0].getValue();
-    UserThread.setUserId(s);
+    Cookie[] cookies = request.getCookies();
+    if (cookies == null) {
+      new Response(response)
+          .setStatus(HttpStatus.UNAUTHORIZED.value())
+          .setContentType(ResponseConstants.CONTENT_TYPE_JSON)
+          .setType(Type.ERROR)
+          .sendMessage(ResponseConstants.UNAUTHORIZED);
+      return false;
+    }
+    UserThread.setUserId(cookies[0].getValue());
     return true;
   }
 
