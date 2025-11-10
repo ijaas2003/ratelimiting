929209d01911ac47fcc9f085b5358b5f3676cadd
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-10 19:35:06 +0530

    Doc change

--- src/main/java/com/ratelimit/ratelimit/Model/UrlWrapper.java
+++ src/main/java/com/ratelimit/ratelimit/Model/UrlWrapper.java
@@ -6,17 +6,34 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
+/**
+ * Wrapper class used for mapping a list of URL configurations
+ * from an XML file. Each URL element represents a rate limiting rule.
+ */
 @JacksonXmlRootElement(localName = "urls")
 public class UrlWrapper {
 
+  /**
+   * List of URL configurations extracted from XML.
+   */
   @JacksonXmlProperty(localName = "url")
   @JacksonXmlElementWrapper(useWrapping = false)
   private List<Url> urls;
 
+  /**
+   * Returns the list of parsed URL configurations.
+   *
+   * @return list of Url objects.
+   */
   public List<Url> getUrlList() {
     return urls;
   }
 
+  /**
+   * Sets the list of URL configurations.
+   *
+   * @param urls List of Url objects to assign.
+   */
   public void setUrlList(List<Url> urls) {
     this.urls = urls;
   }
