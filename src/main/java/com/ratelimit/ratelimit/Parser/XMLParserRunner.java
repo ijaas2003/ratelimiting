81cc0648c5a6f62af45b2818e452fad48133ebd4
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-11 23:36:17 +0530

    Debug Tool Message change

--- src/main/java/com/ratelimit/ratelimit/Parser/XMLParserRunner.java
+++ src/main/java/com/ratelimit/ratelimit/Parser/XMLParserRunner.java
@@ -4,12 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * Runner class that triggers XML parsing when the Spring Boot application
+ * starts.
+ * <p>
+ * Implements {@link CommandLineRunner} to automatically execute the parsing
+ * logic after the application context is fully initialized.
+ */
 @Component
 public class XMLParserRunner implements CommandLineRunner {
 
   @Autowired
   private XMLParser xmlParser;
 
+  /**
+   * Executes on application startup and invokes the XML parser
+   * to load and process the initial XML configuration.
+   *
+   * @param args command-line arguments passed during application startup
+   * @throws Exception if an error occurs while parsing XML
+   */
   @Override
   public void run(String... args) throws Exception {
     xmlParser.parse();
